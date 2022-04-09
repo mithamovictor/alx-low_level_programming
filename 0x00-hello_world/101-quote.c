92784f2034c77f000427480a847ca37b1a2afa49
victor <vickarungaru@gmail.com>
2022-04-09 01:24:03 -0700

    multiple strings

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,14 @@
  * main - main function
  * Return: 1
  */
-int main(void) { 
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+int main(void)
+{
+	int s = strlen(
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			);
+
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
