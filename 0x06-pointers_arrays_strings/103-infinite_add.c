5d6396507de87d35b977947a097f5ba4c0d8d5ac
victor <vickarungaru@gmail.com>
2022-04-25 16:14:03 -0700

    add two numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * char - buffer that the function will use to store the result
+ * Returns: r;
+ **/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
