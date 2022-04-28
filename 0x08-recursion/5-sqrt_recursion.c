f3f9b6d5a5708e739efd042a208de4ed7fc1107e
victor <vickarungaru@gmail.com>
2022-04-28 10:48:14 -0700

    get square root of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,17 +9,17 @@
 
 int _sqrt_recursion(int n)
 {
-	return (_helper(n, 1));
+	return (helper(n, 1));
 }
 
 /**
- * halp - helper function to solve _sqrt_recursion
+ * helper - helper function to solve _sqrt_recursion
  * @c: number to determine if square root
  * @i: incrementer to compare against `c`
  * Return: square root if natural square root, or -1 if none found
  */
 
-int _helper(int c, int i)
+int helper(int c, int i)
 {
 	int square;
 
@@ -27,7 +27,7 @@ int _helper(int c, int i)
 	if (square == c)
 		return (i);
 	else if (square < c)
-		return (_helper(c, i + 1));
+		return (helper(c, i + 1));
 	else
 		return (-1);
 }
