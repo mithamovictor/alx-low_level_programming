0c8b68556f7f5cc5ce91d74fcdb421c267d054e4
victor <vickarungaru@gmail.com>
2022-04-28 10:56:19 -0700

    determine if number is prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int halp(int c, int i);
+int is_prime_number(int n);
+int hai(int n, int d);
+int _sqrt(int x, int i);
